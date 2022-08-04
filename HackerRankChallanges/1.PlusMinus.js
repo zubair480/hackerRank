e1dbd11fd885c2bac1ad62d21eb964a5b720c3f3
zubair480 <zubairzafar480@gmail.com>
2022-08-04 12:48:01 +0500

    second commit.

--- HackerRankChallanges/1.PlusMinus.js
+++ HackerRankChallanges/1.PlusMinus.js
@@ -1,11 +1,13 @@
 // This is my first challenge on HackerRank.
 
 function plusMinus(arr) {
+  // First take initial values of the numbers to be calculated zero.
   let n = arr.length;
   let positive = 0;
   let negative = 0;
   let zero = 0;
   for (let i = 0; i < n; i++) {
+    // Check if number is positive or negative or zero.
     if (n > 0) {
       positive++;
     } else if (n < 0) {
@@ -13,11 +15,13 @@ function plusMinus(arr) {
     } else {
       zero++;
     }
+    // If number is positive, negative or zero, increment the respective counter.
   }
+  // Divide the counters by the length of the array to get the percentage.
   let pos = positive / n;
   let neg = negative / n;
   let zer = zero / n;
-
+  // To print the percentage, we need to convert the number to string.  || \n is used to print on new line.
   console.log(pos.toFixed(6) + "\n" + neg.toFixed(6) + "\n" + zer.toFixed(6));
 }
 
